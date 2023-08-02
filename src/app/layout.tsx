f0568c6415a85f99f6468e6d01734261a8cc2278
Rahul Khanal <rahul39khanal@gmail.com>
2023-08-02 14:14:13 +0545

    navigation initialized

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Banner from '@/components/Banner'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Banner />
+        {children}
+      </body>
     </html>
   )
 }
---
+++ src/components/Banner.tsx
@@ -0,0 +1,70 @@
+"use client"
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+
+const Banner = () => {
+  return (
+    <Carousel
+      className="heroBanner"
+      showStatus={false}
+      autoPlay={true}
+      showArrows={false}
+      infiniteLoop={true}
+    >
+      <div className="heroBanner-container" id="banner" data-aos="zoom-in">
+        <div>
+          <h1>
+            <span>Reach your goals</span> <span> faster</span>
+          </h1>
+        </div>
+        <p>
+          <span> We provide business and brands</span>
+          <span>digital marketing services</span>
+        </p>
+        <div>
+          <a href="#contact">
+            <button>Free SEO Analysis</button>
+          </a>
+        </div>
+      </div>
+      <div className="heroBanner-container">
+        <div>
+          <h1>
+            <span>
+              Connect. Engage.<span>Succeed</span>
+            </span>
+          </h1>
+        </div>
+        <p>
+          <span>Forge connections, engage deeply, and</span>
+          <span>achieve remarkable success together.</span>
+        </p>
+        <div>
+          <a href="#contact">
+            <button>Free SEO Analysis</button>
+          </a>
+        </div>
+      </div>
+      <div className="heroBanner-container">
+        <div>
+          <h1>
+            <span>Elevate Customer</span>
+            <span> Experiences</span>
+          </h1>
+        </div>
+        <p>
+          <span>Revolutionize interactions, elevate satisfaction </span>
+          <span>customer experiences.</span>
+        </p>
+        <div>
+          <a href="#contact">
+            <button>Free SEO Analysis</button>
+          </a>
+        </div>
+      </div>
+    </Carousel>
+  );
+};
+
+export default Banner;
